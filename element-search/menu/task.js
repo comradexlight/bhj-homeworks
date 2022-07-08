fdfa27ecbeef45d0d6b2aca73162cc3ec5c95791
comradexlight <comradexlight@gmail.com>
2022-07-08 17:27:10 +0300

    from work to home

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,8 +1,13 @@
 "use strict"
 
-const allMenuLinks = document.getElementsByClassName("menu__link");
+//const allMenuLinks = document.getElementsByClassName("menu__link");
+const allMenuLinks = document.querySelectorAll(".menu__link");
+//console.log(allMenuLinks);
 
-for (let i = 0; i < Array.from(allMenuLinks).length; i++) {
-	let link = allMenuLinks[i];
-	link.addEventListener("click", console.log(link), false);
+for (let link = 0; link < Array.from(allMenuLinks).length; link++) {
+	allMenuLinks[link].addEventListener("click", function() {
+		return function(){		
+			allMenuLinks[link].nextElementSibling.classList.toggle("menu_active"); //add("menu_active");
+		}
+	}(link))
 };
