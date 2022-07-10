e1cca6019ecf488068fd9819f01804cfa2aaa7c3
comradexlight <comradexlight@gmail.com>
2022-07-10 21:34:45 +0300

    from home to work

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,13 +1,14 @@
 "use strict"
 
-//const allMenuLinks = document.getElementsByClassName("menu__link");
 const allMenuLinks = document.querySelectorAll(".menu__link");
-//console.log(allMenuLinks);
 
-for (let link = 0; link < Array.from(allMenuLinks).length; link++) {
-	allMenuLinks[link].addEventListener("click", function() {
-		return function(){		
-			allMenuLinks[link].nextElementSibling.classList.toggle("menu_active"); //add("menu_active");
-		}
-	}(link))
+for (let i = 0; i < Array.from(allMenuLinks).length; i++) {
+	let link = allMenuLinks[i];
+	if ((link.nextElementSibling !== null) && (link.nextElementSibling.classList.contains("menu__item") !== null))  {
+		link.onclick = () => {
+			console.log("here");
+			link.nextElementSibling.classList.toggle("menu_active");
+			return false
+		};
+	};
 };
