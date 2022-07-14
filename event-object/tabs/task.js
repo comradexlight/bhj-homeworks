f4b12a39f1072a1ce5c84d073b8db1c2230d214c
comradexlight <comradexlight@gmail.com>
2022-07-14 21:15:32 +0300

    hw3

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,12 +1,16 @@
 'use strict'
 
 const allTabs = document.getElementsByClassName("tab");
+const allTabsContent = document.getElementsByClassName("tab__content");
 
 for (let i = 0; i<allTabs.length; i++) {
 	const tab = allTabs[i];
 	tab.addEventListener("click", function(event) {
-		document.getElementsByClassName("tab_active")[0].remove("tab_active");
-		event.target.classList.add("tab_active");
+		document.getElementsByClassName("tab_active")[0].classList.remove("tab_active");
+		tab.classList.add("tab_active");
+
+		document.getElementsByClassName("tab__content_active")[0].classList.remove("tab__content_active");
+		allTabsContent[i].classList.add("tab__content_active");
 	})
 
 };
