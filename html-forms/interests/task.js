1d611ce358f98d05eeef98b359f68458909d7283
comradexlight <comradexlight@gmail.com>
2022-07-21 22:07:58 +0300

    from home to work

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,5 +1,27 @@
 'use strict'
 
-const mainCheckboxList = document.querySelectorAll('li');
+// const allCheckboxList = document.getElementsByClassName('interests interests_active');
+//const allCheckboxList = document.getElementsByClassName('interest__check');
+// console.log(allCheckboxList)
 
-console.log(mainCheckboxList[0].closest('interest__check'));
+// allCheckboxList[0].addEventListener('change', function(event) {
+// 	allCheckboxList[1].checked = event.target.checked;
+// 	allCheckboxList[2].checked = event.target.checked;
+// })
+
+// allCheckboxList[3].addEventListener('change', function(event) {
+// 	allCheckboxList[4].checked = event.target.checked;
+// 	allCheckboxList[5].checked = event.target.checked;
+// })
+
+
+// for (let i = 0; i < allCheckboxList.length; i++) {
+// 	const checkbox = allCheckboxList[i];
+// 	const mainCheckbox = checkbox.previousElementSibling.querySelector('.interest__check');
+// 	mainCheckbox.addEventListener('change', function(event) {
+// 		//event.target.childNodes.checked = true;
+// 		console.log(event)
+// 	})
+	//console.log(checkbox.previousElementSibling.querySelector('.interest__check'));
+//}
+
