6b058fa8fbd3fec69eed4cb73ab743527dcc28d9
comradexlight <comradexlight@gmail.com>
2022-07-22 17:43:03 +0300

    from work to home

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,27 +1,15 @@
 'use strict'
 
-// const allCheckboxList = document.getElementsByClassName('interests interests_active');
-//const allCheckboxList = document.getElementsByClassName('interest__check');
-// console.log(allCheckboxList)
+const mainBlocks = document.getElementsByClassName('interests interests_active');
 
-// allCheckboxList[0].addEventListener('change', function(event) {
-// 	allCheckboxList[1].checked = event.target.checked;
-// 	allCheckboxList[2].checked = event.target.checked;
-// })
-
-// allCheckboxList[3].addEventListener('change', function(event) {
-// 	allCheckboxList[4].checked = event.target.checked;
-// 	allCheckboxList[5].checked = event.target.checked;
-// })
-
-
-// for (let i = 0; i < allCheckboxList.length; i++) {
-// 	const checkbox = allCheckboxList[i];
-// 	const mainCheckbox = checkbox.previousElementSibling.querySelector('.interest__check');
-// 	mainCheckbox.addEventListener('change', function(event) {
-// 		//event.target.childNodes.checked = true;
-// 		console.log(event)
-// 	})
-	//console.log(checkbox.previousElementSibling.querySelector('.interest__check'));
-//}
-
+for (let i = 0; i < mainBlocks.length; i++) {
+	const block = mainBlocks[i];
+	const mainCheckbox = block.previousElementSibling.querySelector('.interest__check');
+	const littleCheckboxList = block.querySelectorAll('.interest__check');
+	
+	mainCheckbox.addEventListener('change', function() {
+		littleCheckboxList.forEach((element) => {
+			element.checked = mainCheckbox.checked;
+		})
+	})
+}
