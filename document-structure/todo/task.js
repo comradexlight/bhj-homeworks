2ad109e1f1278a1b9ebbc5674b92f90ba7cffc3d
comradexlight <comradexlight@gmail.com>
2022-07-25 21:54:17 +0300

    from home to work

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -2,22 +2,44 @@
 
 const taskInput = document.getElementById('task__input');
 const tasksAdd = document.getElementById('tasks__add');
+const tasksList = document.getElementById('tasks__list');
+//const closeButtonsList = document.querySelectorAll('.task__remove');
 
-tasksAdd.addEventListener('click')
+function addTask() {
+	const task = `
+				<div class="task">
+	  				<div class="task__title">
+	    				${taskInput.value}
+	  				</div>
+	  				<a href="#" class="task__remove">&times;</a>
+				</div>
+				`
 
+	tasksList.insertAdjacentHTML('afterend', task);
 
-// taskInput.addEventListener('keyup', function(event) {
+}
 
-// 	if (event.key === 'Enter' && taskInput.value !== '' && taskInput.value !== ' ') {
-// 		console.log(event.key)
-// 		// const task = `
-// 		// 			<div class="task">
-// 		// 		  		<div class="task__title">
-// 		// 		    		${taskInput.value}
-// 		// 		  		</div>
-// 		// 		  		<a href="#" class="task__remove">&times;</a>
-// 		// 			</div>
-// 		// 			`
-// 		// console.log(task)
-// 	}
-// })
+function clearTaskInput() {
+	taskInput.value = '';
+};
+
+tasksAdd.onclick = function(event) { event.preventDefault() };
+
+taskInput.addEventListener('keydown', function(event) {
+	if (event.key === 'Enter' && taskInput.value && taskInput.value !== ' ') {
+		addTask();
+		clearTaskInput();
+	}
+})
+
+// function removeTask(task) {
+// 	console.log(task.closest('.task'))
+// }
+
+// for (let i = 0; i < closeButtonsList.length; i++) {
+// 	const closeButton = closeButtonsList[i];
+// 	closeButton.addEventListener('click', function(event) {
+// 		console.log(event.target)
+// 		return false
+// 	})
+// };
